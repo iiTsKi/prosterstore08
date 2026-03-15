4ab8a3b9db826304ee697f810797cd91f0b9d27e
iiTsKi <itski.arthur@gmail.com>
2026-03-15 16:10:15 +0800

    prisma_init

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,15 @@
 import sampleData from "./sample-data";
+import { PrismaClient } from "@/lib/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 async function main() {
+  const prisma = new PrismaClient({ adapter });
+  await prisma.product.deleteMany({});
+  await prisma.product.createMany({ data: sampleData.products });
+
   console.log("Database seeded successfully!");
 }
 

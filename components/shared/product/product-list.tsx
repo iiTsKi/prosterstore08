4dc50d8bf9f1dfc9a2fcefc970f74ffa7a22e2cd
iiTsKi <itski.arthur@gmail.com>
2026-03-15 14:18:13 +0800

    initcommit

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -13,7 +13,7 @@ const ProductList = ({
   return (
     <>
       <div className="my-10">
-        <h2 className="h2-bold mb-4">{title}</h2>
+        <h2 className="h2-bold mb-5">{title}</h2>
         {data.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
             {limitedData.map((product: any) => (
